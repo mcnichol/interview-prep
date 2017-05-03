5e81811809bfce668d2f6e98e28646b9b4d17926
Michael <mcnichol.m@gmail.com>
2017-05-02 22:31:35 -0500

    implementation of insertion sort

--- src/main/java/com/mcnichol/interviewing/sorting/InsertionSort.java
+++ src/main/java/com/mcnichol/interviewing/sorting/InsertionSort.java
@@ -4,5 +4,15 @@ public class InsertionSort implements Sorter {
     @Override
     public void sort(Integer[] objectToSort) {
 
+        for (int i = 0; i < objectToSort.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (objectToSort[j] < objectToSort[j - 1]) {
+                    int temp = objectToSort[j];
+                    objectToSort[j] = objectToSort[j - 1];
+                    objectToSort[j - 1] = temp;
+                } else break;
+
+            }
+        }
     }
 }

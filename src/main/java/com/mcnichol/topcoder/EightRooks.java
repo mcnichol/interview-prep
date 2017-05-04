d5250399323089a3fb862c77255371ad59ae4d79
Michael <mcnichol.m@gmail.com>
2017-05-04 11:37:41 -0500

    Use javadoc for EightRooks details

--- src/main/java/com/mcnichol/topcoder/EightRooks.java
+++ src/main/java/com/mcnichol/topcoder/EightRooks.java
@@ -1,93 +1,92 @@
 package com.mcnichol.topcoder;
 
-
-/*
-Problem Statement:
-Cat Snuke is playing a puzzle called Eight Rooks Puzzle. In this puzzle, he must put exactly eight rooks onto
-an 8x8 chessboard. No two rooks must be in the same row, and no two rooks must be in the same column.
-
-You are given a String[] board. The j-th character of the i-th element of board is either 'R' or '.'. If this is 'R',
-the cell in row i, column j contains a rook. Otherwise the cell doesn't contain a rook.
-
-Determine whether Snuke solved the puzzle correctly. If he solved the puzzle correctly, return "Correct". Otherwise
-return "Incorrect".
-
-Definition:
-    Class:	EightRooks
-    Method:	isCorrect
-    Parameters:	String[]
-    Returns:	String
-    Method signature:	String isCorrect(String[] board)
-    (be sure your method is public)
-
-Constraints:
--	board will contain exactly 8 elements.
--	Each element of board will contain exactly 8 characters.
--	Each character in board will be either 'R' or '.'.
-
-Examples:
-0)
-
-{"R.......",
- ".R......",
- "..R.....",
- "...R....",
- "....R...",
- ".....R..",
- "......R.",
- ".......R"}
-Returns: "Correct"
-1)
-
-{"........",
- "....R...",
- "........",
- ".R......",
- "........",
- "........",
- "..R.....",
- "........"}
-Returns: "Incorrect"
-The number of rooks is not eight.
-2)
-
-{"......R.",
- "....R...",
- "...R....",
- ".R......",
- "R.......",
- ".....R..",
- "..R.....",
- ".......R"}
-Returns: "Correct"
-3)
-
-{"......R.",
- "....R...",
- "...R....",
- ".R......",
- "R.......",
- ".......R",
- "..R.....",
- ".......R"}
-Returns: "Incorrect"
-The rightmost column contains two rooks.
-4)
-
-{"........",
- "........",
- "........",
- "........",
- "........",
- "........",
- "........",
- "........"}
-Returns: "Incorrect"
-
-Disclaimer:
-This problem statement is the exclusive and proprietary property of TopCoder, Inc. Any unauthorized use or
-reproduction of this information without the prior written consent of TopCoder, Inc. is strictly prohibited.
-(c)2010, TopCoder, Inc. All rights reserved.
+/**
+ * Problem Statement:
+ * Cat Snuke is playing a puzzle called Eight Rooks Puzzle. In this puzzle, he must put exactly eight rooks onto
+ * an 8x8 chessboard. No two rooks must be in the same row, and no two rooks must be in the same column.
+ * <p>
+ * You are given a String[] board. The j-th character of the i-th element of board is either 'R' or '.'. If this is 'R',
+ * the cell in row i, column j contains a rook. Otherwise the cell doesn't contain a rook.
+ * <p>
+ * Determine whether Snuke solved the puzzle correctly. If he solved the puzzle correctly, return "Correct". Otherwise
+ * return "Incorrect".
+ * <p>
+ * Definition:
+ * Class:	EightRooks
+ * Method:	isCorrect
+ * Parameters:	String[]
+ * Returns:	String
+ * Method signature:	String isCorrect(String[] board)
+ * (be sure your method is public)
+ * <p>
+ * Constraints:
+ * -	board will contain exactly 8 elements.
+ * -	Each element of board will contain exactly 8 characters.
+ * -	Each character in board will be either 'R' or '.'.
+ * <p>
+ * Examples:
+ * 0)
+ * <p>
+ * {"R.......",
+ * ".R......",
+ * "..R.....",
+ * "...R....",
+ * "....R...",
+ * ".....R..",
+ * "......R.",
+ * ".......R"}
+ * Returns: "Correct"
+ * 1)
+ * <p>
+ * {"........",
+ * "....R...",
+ * "........",
+ * ".R......",
+ * "........",
+ * "........",
+ * "..R.....",
+ * "........"}
+ * Returns: "Incorrect"
+ * The number of rooks is not eight.
+ * 2)
+ * <p>
+ * {"......R.",
+ * "....R...",
+ * "...R....",
+ * ".R......",
+ * "R.......",
+ * ".....R..",
+ * "..R.....",
+ * ".......R"}
+ * Returns: "Correct"
+ * 3)
+ * <p>
+ * {"......R.",
+ * "....R...",
+ * "...R....",
+ * ".R......",
+ * "R.......",
+ * ".......R",
+ * "..R.....",
+ * ".......R"}
+ * Returns: "Incorrect"
+ * The rightmost column contains two rooks.
+ * 4)
+ * <p>
+ * {"........",
+ * "........",
+ * "........",
+ * "........",
+ * "........",
+ * "........",
+ * "........",
+ * "........"}
+ * Returns: "Incorrect"
+ * <p>
+ * Disclaimer:
+ * This problem statement is the exclusive and proprietary property of TopCoder, Inc. Any unauthorized use or
+ * reproduction of this information without the prior written consent of TopCoder, Inc. is strictly prohibited.
+ * (c)2010, TopCoder, Inc. All rights reserved.
  */
 public class EightRooks {
 
